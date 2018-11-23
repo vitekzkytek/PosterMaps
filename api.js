32228e75592af43d9442de96917adc04c180a10f
vitekzkytek <7482296+vitekzkytek@users.noreply.github.com>
2018-11-23 23:59:00 +0100

    Update api.js

--- api.js
+++ api.js
@@ -2,6 +2,8 @@ var mymap;
 
 
 function DrawMap() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoidml0ZWt6a3l0ZWsiLCJhIjoiY2pvdTYyZWE4MThvOTNwbXpkcG40YTEyeCJ9.NYfKgDcmwcTw5zo8u4UFmg';
+
     mymap = new mapboxgl.Map({
     container: 'mapid', // container id
     style: 'mapbox://styles/mapbox/dark-v9', //hosted style id
